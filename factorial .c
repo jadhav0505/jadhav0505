e7f6c637b1f47080bd8252070b09f292ce97dfc3
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-19 10:35:13 +0530

    using for loop

---
+++ factorial .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ int i,fact=1;number;
+ printf("\n Enter the no. :");
+ scanf("%d",number);
+ for(i=1;i<=number;i++)
+ {
+  fcat=fcat*1;
+ }
+  printf("\n Factorial  is=%d",fcat);
+  return 0;
+}
+ 
