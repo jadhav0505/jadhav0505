1ce2d55c166b6b4337373ed4b592692e7ca21e54
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-22 15:09:28 +0530

    using for loop

---
+++ find position of array element .c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[i],i;
+  clrscr();
+  printf("\n enter the element:");
+  for(i=0;i<5;i++)
+  {
+    scanf("&d",&a[i]);
+  }
+  for(i=0;i<5;i++)
+  {
+    printf("\n Element of=%d \t position=%d",i,a[i]);
+  }
+  getch();
+}
+  
