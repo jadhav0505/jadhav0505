0bd530fb8715c6c5ecef7c4208b13dc93657bcc2
jadhav0505 <119648693+jadhav0505@users.noreply.github.com>
2022-12-29 23:51:50 +0530

    using for loop

---
+++ 2 D Array Matrix .c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int a[3][3],i,j;
+  clrscr();
+  printf("\n Enter the number :");
+  for(i=0;i<3;i++)
+  {
+    for(j=0;j<3;j++)
+    {
+      scanf("%d",&a[i][j]);
+    }
+  }
+  prinmtf("\n 2D Array Matrix :\n");
+  for(i=0;i<3;i++)
+  {
+    for(j=0;j<3;j++)
+    {
+      printf(%d\y",a[i][j]);
+             }
+             printf("\n");
+             }
+ getch();
+}
